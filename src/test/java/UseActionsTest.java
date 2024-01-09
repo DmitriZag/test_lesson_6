a4dae99db6690a77f005ccb707f7afe464733e1b
Dmitry <dmitryzag88@gmail.com>
2024-01-09 21:59:24 +0300

    new tests added

---
+++ src/test/java/UseActionsTest.java
@@ -0,0 +1,26 @@
+package guru.qa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.text;
+
+
+public class UseActionsTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void fillUseActionsTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(200, 0).release().perform();
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+
+    }
+
+}
